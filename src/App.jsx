cd082f797991a33f7435c1c05db6ba441afa9c21
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-24 20:02:29 +0530

    added files in Shared

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route,  } from 'react-router-dom'
 import Users from './users/pages/Users'
 import './App.css'
-import NewPlace from './places/components/NewPlace'
+import NewPlace from './places/components/NewPlace';
+import MainNavigation from './shared/components/UIElements/Navigation/MainNavigation';
 
 function App() {
  
   return (
     <>
+    <MainNavigation/>
     <Routes>
       <Route path="/" element={<Users/>} exact/>
       <Route path="/places/new" element={<NewPlace/>} exact/>
---
+++ src/shared/components/UIElements/Navigation/MainNavigation.jsx
@@ -0,0 +1,20 @@
+//import React from 'react'
+import { Link } from "react-router-dom";
+import MainHeader from "./MainHeader";
+import "./MainNavigation.css"
+
+const MainNavigation = (props) => {
+    return (
+        <MainHeader>
+            <button className="main-nvigation__menu-btn">
+                <span /> <span /> <span />
+            </button>
+            <h1>
+                <Link to="/">Your Places</Link>
+            </h1>
+
+        </MainHeader>
+    )
+}
+
+export default MainNavigation
