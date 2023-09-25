13e867a22f99511f05859db439d41329e219d8a1
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-25 10:52:19 +0530

    added Drawer

--- src/shared/components/UIElements/Navigation/MainNavigation.jsx
+++ src/shared/components/UIElements/Navigation/MainNavigation.jsx
@@ -1,20 +1,31 @@
-//import React from 'react'
+import React,{useState} from 'react'
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../Backdrop/Backdrop";
 import "./MainNavigation.css"
 
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsopen] = useState(false)
+    const openDrawerHandler = () => {
+        setDrawerIsopen(true);
+    }
+    const closeDrawerHandler = () => {
+        setDrawerIsopen(false)
+    }
     return (
         <>
-        <SideDrawer>
+        {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
+        
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
             <nav className="main-navigation__drawer-nav">
                 <NavLinks/>
             </nav>
         </SideDrawer>
+}
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                 <span /> <span /> <span />
             </button>
             <h1 className="main-navigation__title">
--- src/shared/components/UIElements/Navigation/SideDrawer.jsx
+++ src/shared/components/UIElements/Navigation/SideDrawer.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { createPortal } from 'react-dom';
+import { CSSTransition } from 'react-transition-group'
 import './SideDrawer.css';
 
 
 const SideDrawer = (props) => {
-    const content=  <aside className='side-drawer'>{props.children}</aside>;
-  return createPortal(content, document.getElementById('drawer-hook'))
+    const content = (
+        <CSSTransition
+        in={props.show}
+        timeout={200}
+        classNames={'slide-in-left'}
+        mountOnEnter
+        unmountOnExit
+        >
+            <aside onClick={props.onClick} className='side-drawer'>
+                {props.children}
+            </aside>
+        </CSSTransition>
+    )
+    return createPortal(content, document.getElementById('drawer-hook'))
 }
 
 export default SideDrawer
