755627157a7d042f5fd17b4b5dd55bbe9eea5558
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-25 20:36:22 +0530

    added google maps

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Map from '../../shared/components/UIElements/Map';
+//import Map from '../../shared/components/UIElements/Map';
 import Card from '../../shared/components/UIElements/Card/Card';
 import Button from '../../shared/components/FormElements/Button/Button';
 import Modal from '../../shared/components/UIElements/Modal';
@@ -7,15 +7,15 @@ import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
-  const [showMap, setShowMap] = useState(false);
+    const [showMap, setShowMap] = useState(false);
 
-  const openMapHandler = () => setShowMap(true);
+    const openMapHandler = () => setShowMap(true);
 
-  const closeMapHandler = () => setShowMap(false);
+    const closeMapHandler = () => setShowMap(false);
 
-  return (
-    <React.Fragment>
-      <Modal
+    return (
+        <React.Fragment>
+            {/* <Modal
         show={showMap}
         onCancel={closeMapHandler}
         header={props.address}
@@ -26,26 +26,54 @@ const PlaceItem = props => {
         <div className="map-container">
           <Map center={props.coordinates} zoom={16} />
         </div>
-      </Modal>
-      <li className="place-item">
-        <Card className="place-item__content">
-          <div className="place-item__image">
-            <img src={props.image} alt={props.title} />
-          </div>
-          <div className="place-item__info">
-            <h2>{props.title}</h2>
-            <h3>{props.address}</h3>
-            <p>{props.description}</p>
-          </div>
-          <div className="place-item__actions">
-            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-            <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
-          </div>
-        </Card>
-      </li>
-    </React.Fragment>
-  );
+      </Modal> */}
+            <Modal
+                show={showMap}
+                onCancel={closeMapHandler}
+                header={props.address}
+                contentClass="place-item__modal-content"
+                footerClass="place-item__modal-actions"
+                footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+
+            >
+                <div className="map-container" >
+                    <iframe
+                        title="map"
+                        width="100%"
+                        height="100%"
+                        frameBorder="0"
+                        scrolling="no"
+                        marginHeight="0"
+                        marginWidth="0"
+                        // src={'https://maps.google.com/maps?q=' + props.coordinates.lat.toString() + ',' + props.coordinates.lng.toString() + '&t=&z=15&ie=UTF8&iwloc=&output=embed'}
+                        src={`https://maps.google.com/maps?q=${props.coordinates.lat.toString() + ',' + props.coordinates.lng.toString()}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+
+                    >
+                    </iframe>
+                </div>
+
+
+            </Modal>
+
+            <li className="place-item">
+                <Card className="place-item__content">
+                    <div className="place-item__image">
+                        <img src={props.image} alt={props.title} />
+                    </div>
+                    <div className="place-item__info">
+                        <h2>{props.title}</h2>
+                        <h3>{props.address}</h3>
+                        <p>{props.description}</p>
+                    </div>
+                    <div className="place-item__actions">
+                        <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+                        <Button to={`/places/${props.id}`}>EDIT</Button>
+                        <Button danger>DELETE</Button>
+                    </div>
+                </Card>
+            </li>
+        </React.Fragment>
+    );
 };
 
 export default PlaceItem;
