c6c73129f9cd411ead9f262e1cd06dfc3e4f94b0
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-10-15 19:54:19 +0530

    managing state in Input

--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,12 +1,34 @@
-// import React from 'react';
+import {useReducer} from 'react';
 import "./Input.css";
 
+
+const inputReducer = (state, action) => {
+  switch(action.type){
+    default state
+  }
+
+}
 const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {value:'', isValid: false})
+
+  const changeHandler = () => {
+
+  }
 
     const element =
     props.element === 'input' ? (
-    <input id={props.id} type={props.type} placeholder={props.placeholder}/>) : (
-<textarea id={props.id} rows={props.roe || 3}/>
+    <input 
+    id={props.id} 
+    type={props.type} 
+    placeholder={props.placeholder}
+    onChange={changeHandler}
+    
+    />) : (
+<textarea 
+id={props.id} 
+rows={props.roe || 3}
+onChange={changeHandler}
+/>
     )
   return (
     <div className={`form-control`}>
